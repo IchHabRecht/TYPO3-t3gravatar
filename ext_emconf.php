a8f10763ee0023afa48aa2f48c4f810417f14651
Frans Saris <frans@beech.it>
2015-08-14 19:04:21 +0200

    [FEATURE] Add composer.json and increase version
    Also cleaned ext_emconf.php and added Readme.rst

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,12 +2,6 @@
 
 /***************************************************************
  * Extension Manager/Repository config file for ext "t3gravatar".
- *
- * Auto generated 13-08-2015 12:11
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
@@ -17,24 +11,23 @@ $EM_CONF[$_EXTKEY] = array (
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
+	'clearCacheOnLoad' => 1,
 	'author' => 'Nicole Cordes',
 	'author_email' => 'typo3@cordes.co',
 	'author_company' => 'CPS-IT GmbH',
-	'version' => '0.2.1',
-	'constraints' => 
+	'version' => '0.3.0',
+	'constraints' =>
 	array (
-		'depends' => 
+		'depends' =>
 		array (
 			'typo3' => '7.4.0-7.99.99',
 		),
-		'conflicts' => 
+		'conflicts' =>
 		array (
 		),
-		'suggests' => 
+		'suggests' =>
 		array (
 		),
-	),
-	'_md5_values_when_last_written' => 'a:4:{s:9:"ChangeLog";s:4:"e839";s:12:"ext_icon.png";s:4:"130e";s:17:"ext_localconf.php";s:4:"1719";s:25:"Classes/Xclass/Avatar.php";s:4:"09f3";}',
+	)
 );
 
